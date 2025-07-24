c25a09487b6cf8469f91e3149c7a09466ac49fe6
edieehuo <ediehuo05@gmail.com>
2025-07-24 16:58:57 -0400

    Set Up Screens and Transitions
    Tried to set up screens and transitions. Some issues with how to preserve data, some confusion with OOP, major confusion with importing the right functions from different files.

---
+++ gambling.py
@@ -0,0 +1,11 @@
+from cmu_graphics import *
+def gambling_redrawAll(app):
+    drawLabel("Gambling Screen", app.width/2, 200, size=40, bold=True)
+    drawLabel("Press N for numbers game", app.width/2, app.height/2)
+    drawLabel("Press L for Lottery", app.width/2, app.height/2 + 50)
+
+def gambling_onKeyPress(app, key):
+    if key == 'n':
+        setActiveScreen('numbersGame')
+    else:
+        pass  # Ignore other keys
--- main.py
+++ main.py
@@ -1,50 +1,56 @@
+#---Baby 2 Billionaire Game--------------------------------------------------------------------------------------
 from cmu_graphics import *
-from welcomeScreen import *
+from player import *
+from hamster import *
+from gambling import *
+from investment import *
+#importing is not working! 
+# print(hamster_redrawAll) 
 
-def onAppStart(app):
-    app.mode = 'welcome'  # Initial mode
-    app.width = 800
-    app.height = 600
-    app.player = None  # Placeholder for player object
-    app.score = 0  # Initialize score for the game
-    app.gameOver = False  # Flag to check if the game is over
+#---Start Screen--------------------------------------------------------------------------------------
+def start_onScreenActivate(app):
+    app.gameState = False  # Initialize game state to False
+    app.player = None  
 
-def redrawAll(app):
-    if app.mode == 'welcome':
-        drawWelcomeScreen(app)
-    elif app.mode == 'game':
-        drawGameScreen(app)
-    elif app.mode == 'gameOver':
-        drawGameOverScreen(app)
+def start_redrawAll(app):
+    drawLabel("Baby 2 Billionaire", app.width/2, 200, size=40, bold=True)
+    drawLabel("Press any key to start", app.width/2, 300)
+
+def start_onKeyPress(app, key):
+    setActiveScreen('spawn')
+
+#---Spawn Screen--------------------------------------------------------------------------------------
+def spawn_redrawAll(app):
+    drawLabel("Choose Your Path", app.width/2, 200, size=40, bold=True)
+    player = player(app.name, 0, rand(1000, 10000), rand(10000, 100000)) 
+    drawLabel("Press S to start Game", app.width/2, 300)
+
+def spawn_onKeyPress(app, key):
+    if key == 's':
+        app.gameState = True 
+        setActiveScreen('decision')
     else:
-        pass
+        pass  # Ignore other keys
 
-def resetApp(app):
-    app.mode = 'welcome'
-    app.player = None
-    app.score = 0
-    app.gameOver = False
+#---Decision Screen--------------------------------------------------------------------------------------
 
-#---Event Handlers-----------------------------------------------------------------------------------
-def onKeyPress(app, key):
-    if app.mode == 'game':
-        if key == 'r':  # Restart game
-            app.mode = 'game'
-        elif key == 'q':  # Quit game
-            app.mode = 'welcome'
+def decision_redrawAll(app):
+    drawLabel("Next Choice To Make", app.width/2, 200, size=40, bold=True)
+    drawLabel("Press H for Hamster Wheel", app.width/2, 300)
+    drawLabel("Press G for Gambling", app.width/2, 350)
+    drawLabel("Press I for Investment", app.width/2, 400)
 
-
-#---Drawing Functions--------------------------------------------------------------------------------
-def drawWelcomeScreen(app):
-    pass 
-
-def drawGameScreen(app):
-    pass
-
-def drawGameOverScreen(app):
-    pass 
+def decision_onKeyPress(app, key):
+    if key == 'h':
+        setActiveScreen('hamster')
+    elif key == 'g':
+        setActiveScreen('gambling')
+    elif key == 'i':
+        setActiveScreen('investment')
+    else:
+        pass # Ignore other keys
 
 #---Don't Touch--------------------------------------------------------------------------------------
 def main():
-    runApp()
+    runAppWithScreens(initialScreen='start', width=800, height=600)
 main()
