e6872bb86b661242c8d4ee5c0d8e95e0e1495b8d
edieehuo <ediehuo05@gmail.com>
2025-07-24 16:26:52 -0400

    Setting Up Main File
    Started to separate out where I want big chunks of logic to go

--- main.py
+++ main.py
@@ -1,5 +1,50 @@
 from cmu_graphics import *
+from welcomeScreen import *
 
+def onAppStart(app):
+    app.mode = 'welcome'  # Initial mode
+    app.width = 800
+    app.height = 600
+    app.player = None  # Placeholder for player object
+    app.score = 0  # Initialize score for the game
+    app.gameOver = False  # Flag to check if the game is over
+
+def redrawAll(app):
+    if app.mode == 'welcome':
+        drawWelcomeScreen(app)
+    elif app.mode == 'game':
+        drawGameScreen(app)
+    elif app.mode == 'gameOver':
+        drawGameOverScreen(app)
+    else:
+        pass
+
+def resetApp(app):
+    app.mode = 'welcome'
+    app.player = None
+    app.score = 0
+    app.gameOver = False
+
+#---Event Handlers-----------------------------------------------------------------------------------
+def onKeyPress(app, key):
+    if app.mode == 'game':
+        if key == 'r':  # Restart game
+            app.mode = 'game'
+        elif key == 'q':  # Quit game
+            app.mode = 'welcome'
+
+
+#---Drawing Functions--------------------------------------------------------------------------------
+def drawWelcomeScreen(app):
+    pass 
+
+def drawGameScreen(app):
+    pass
+
+def drawGameOverScreen(app):
+    pass 
+
+#---Don't Touch--------------------------------------------------------------------------------------
 def main():
     runApp()
 main()
