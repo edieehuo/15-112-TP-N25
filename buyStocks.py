1706f818ccf8d6c2c63789daf5939f7a48c0c92b
edieehuo <ediehuo05@gmail.com>
2025-07-26 23:46:35 -0400

    Fixed Buying!
    Ran into problem now where i set things to be <100 and then i printed ,000 on the screen. can't be buying stocks like that...

--- buyStocks.py
+++ buyStocks.py
@@ -13,6 +13,8 @@ from sellStocks import *
 
 def buyStocks_onScreenActivate(app):
     app.buyStockNum = None  # Initialize as None, will wait for user input
+    app.tooBroke = False 
+    pass 
 
 def buyStocks_redrawAll(app):
     drawLabel("BUY STOCKS SCREEN", app.width / 2, 10, size=40, bold=True)
@@ -21,18 +23,35 @@ def buyStocks_redrawAll(app):
         drawLabel("Choose between 0-9 to buy", app.width / 2, app.height / 2, size=30, align='center')
     else:
         drawLabel(f'Number of stocks to buy: {app.buyStockNum}', app.width / 2, app.height / 2, size=20, bold=True)
-        drawLabel(f'Press b to buy. Remember, no backsies!', app.width / 2, app.height / 2 + 20, size=20, bold=True)
+        drawLabel(f'Remember, no backsies! Press b to buy {app.buyStockNum} stocks.', app.width / 2, app.height / 2 + 20, size=20, bold=True)
         if app.playerPortfolio != None:
             drawLabel(f'Your portfolio: {app.playerPortfolio}', app.width / 2, app.height / 2 + 50, size=20, align='center')
+    if app.tooBroke:
+        drawLabel(f'Too broke to buy {app.buyStockNum} stocks.', app.width / 2, app.height / 2 + 80, size=20, bold=True)
 
 def buyStocks_onKeyPress(app, key):
     if key.isdigit():  # Check if the key pressed is a digit (number)
         app.buyStockNum = int(key)
-    elif key == 'b' and app.buyStockNum is not None:  # If 'b' is pressed and buyStockNum is valid
-        # Add the stock using the input number of stocks
-        app.playerPortfolio.addStock(app.stockInfo.stockPrice, app.stockInfo.stockVolatility, app.buyStockNum)
-        print(app.playerPortfolio)
-        setActiveScreen('decision')
-    elif key == 'space':
-        setActiveScreen('decision')  # Move to the next screen when space is pressed
+        app.tooBroke = False 
 
+    elif key == 'b' and  app.buyStockNum is not None:
+        if canBuy(app):  
+            app.tooBroke = False 
+            app.money -= app.stockInfo.stockPrice*app.buyStockNum 
+            app.playerPortfolio.addStock(app.stockInfo.stockPrice, app.stockInfo.stockVolatility, app.buyStockNum)
+            print(app.playerPortfolio)
+            setActiveScreen('decision')
+        if not canBuy(app): 
+            app.tooBroke = True 
+    else:
+        pass
+
+def canBuy(app): 
+    print('buyStocks: canBuy', app.buyStockNum, app.stockInfo.stockPrice)
+    value = app.buyStockNum * app.stockInfo.stockPrice
+    print('buyStocks: canBuy', value)
+    print('buyStocks: canBuy', app.player.money) 
+    if value <= app.player.money*1000:
+        print(f'has this much {app.player.money},000 , want to buy {value} amount stocks')
+        return True 
+    return False 
